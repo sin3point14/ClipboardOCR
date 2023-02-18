ee8c4750289da2b7cda7c9ea1d9c1869e956073c
sin3point14 <manaschaudhary2000@gmail.com>
2023-02-18 18:58:11 +0530

    Run process as daemon

--- ClipboardOCR/Program.cs
+++ ClipboardOCR/Program.cs
@@ -1,10 +1,20 @@
-﻿using System.Windows.Forms;
-using Tesseract;
+﻿using Tesseract;
 
 internal class Program
-{
+{    
     [STAThreadAttribute]
     private static void Main(string[] args)
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new TrayDaemon());
+    }
+}
+
+public class TrayDaemon : ApplicationContext
+{
+    private NotifyIcon trayIcon;
+    private static void ClipboardOCR(object? sender, EventArgs e)
     {
         if (Clipboard.ContainsImage())
         {
@@ -14,9 +24,34 @@ internal class Program
                 using (var page = engine.Process(img))
                 {
                     var text = page.GetText();
-                    Console.WriteLine("Text found: {0}", text);
+                    Clipboard.SetText(text);
                 }
             }
-        } 
+        }
+    }
+
+    void Exit(object? sender, EventArgs e)
+    {
+        // Hide tray icon, otherwise it will remain shown until user mouses over it
+        trayIcon.Visible = false;
+
+        Application.Exit();
+    }
+
+    public TrayDaemon()
+    {
+        trayIcon = new NotifyIcon()
+        {
+            Icon = new NotifyIcon().Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath),
+            ContextMenuStrip = new ContextMenuStrip()
+            {
+                Items =
+                {
+                    new ToolStripMenuItem("Get Text", null, ClipboardOCR, "Get Text"),
+                    new ToolStripMenuItem("Exit", null, Exit, "Exit"),
+                }
+            },
+            Visible = true
+        };
     }
 }
