dafab44b7e21c7c2ca380b012bea59c4021aa4ab
sin3point14 <manaschaudhary2000@gmail.com>
2023-02-18 18:01:36 +0530

    Read Image from clipboard and OCR

---
+++ ClipboardOCR/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using Tesseract;
+
+internal class Program
+{
+    [STAThreadAttribute]
+    private static void Main(string[] args)
+    {
+        if (Clipboard.ContainsImage())
+        {
+            var img = (Bitmap)Clipboard.GetImage();
+            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            {
+                using (var page = engine.Process(img))
+                {
+                    var text = page.GetText();
+                    Console.WriteLine("Text found: {0}", text);
+                }
+            }
+        } 
+    }
+}
