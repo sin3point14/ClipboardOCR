cafab7dc41666382c1d970711c51a2387802aecc
sin3point14 <manaschaudhary2000@gmail.com>
2023-02-18 17:19:27 +0530

    Initialise clipboard API

---
+++ ClipboardTesseract/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+internal class Program
+{
+    [STAThreadAttribute]
+    private static void Main(string[] args)
+    {
+        Clipboard.SetText("hello");
+    }
+}
